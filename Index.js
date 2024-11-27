da80a1f316ab96c2ebaf0bbee58d5aebb53277cd
alex-pashayev <124063603+alex-pashayev@users.noreply.github.com>
2024-11-27 13:21:03 +0200

    create new function for tow rand nubers

--- Index.js
+++ Index.js
@@ -1,2 +1,12 @@
-var random_num1 = Math.floor(Math.random()*6)+1;
-var random_num2 = Math.floor(Math.random()*6)+1;
+
+function TowRandNums ()
+{
+    var random_num1 = Math.floor(Math.random()*6)+1;
+    var random_num2 = Math.floor(Math.random()*6)+1;
+    var arr =[random_num1,random_num2];
+    return arr ;
+}
+var arr = TowRandNums ();
+alert("firs num is "+arr[0]+" and the econe one is "+arr[1]);
+
+
