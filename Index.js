86df2094eeb9ba0fedc2a149dbba0d8ee0cf2eb9
alex-pashayev <124063603+alex-pashayev@users.noreply.github.com>
2024-11-28 02:13:33 +0200

    deleting an unnecesary function

--- Index.js
+++ Index.js
@@ -4,33 +4,14 @@ function TowRandNums ()
     var random_num1 = Math.floor(Math.random()*6)+1;
     var random_num2 = Math.floor(Math.random()*6)+1;
     var arr =[random_num1,random_num2];
+    document.querySelector("#Img1").setAttribute("src","./images/dice"+arr[0]+".png");
+    document.querySelector("#Img2").setAttribute("src","./images/dice"+arr[1]+".png");
     return arr ;
-}
-
-function changeNumbers(Arr)
-{   
-    for(var i =1 ;i<7;i++)
-    {
-        
-        if(Arr[0]==i){
-            document.querySelector("#Img1").setAttribute("src","./images/dice"+i+".png");
-            break;
-        }
-    }
-
-    for(var i =1 ;i<7;i++)
-        {
-            if(Arr[1]==i){
-                document.querySelector("#Img2").setAttribute("src","./images/dice"+i+".png");
-                break;
-            }
-        } 
 }
 
 function StartDiceeGame()
 {
     var randNumber = TowRandNums ();
-    changeNumbers(randNumber);
     if(randNumber[0]>randNumber[1])
     {
         var a =document.querySelector("h1").innerText="the winner is player one";
